e665fdbbca35849ae0b1206f8f29ad4f85de4670
Charlie Snell <sea_snell@icloud.com>
2023-04-19 18:42:50 +0000

    merge

--- tpu_vm_setup.sh
+++ tpu_vm_setup.sh
@@ -20,7 +20,7 @@ source ~/miniconda3/bin/activate
 conda init bash
 conda env create -f environment.yml
 conda activate JaxSeq2
-python -m pip install --upgrade pip && python -m pip install jax[tpu]==0.4.8 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python -m pip install --upgrade pip && python -m pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 # clean up
 rm -rf ~/Miniconda3-py310_23.1.0-1-Linux-x86_64.sh
